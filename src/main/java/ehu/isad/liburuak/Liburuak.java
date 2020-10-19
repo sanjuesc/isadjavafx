244e15de4862a795954b1c1e50dab9a6dfb21632
ander <elsanjuyt@gmail.com>
2020-10-19 11:33:06 +0200

    Tamainak aldatu

--- src/main/java/ehu/isad/liburuak/Liburuak.java
+++ src/main/java/ehu/isad/liburuak/Liburuak.java
@@ -32,8 +32,8 @@ public class Liburuak extends Application {
         stage = primaryStage;
         pantailakKargatu();
 
-        libKudScene = new Scene(liburuUI,600,500);
-        xeheScene = new Scene(xeheUI,600,500);
+        libKudScene = new Scene(liburuUI,600,400);
+        xeheScene = new Scene(xeheUI,800,500);
 
         stage.setTitle("Liburuak ariketa");
         stage.setScene(libKudScene);
@@ -68,6 +68,7 @@ public class Liburuak extends Application {
         stage.setScene(xeheScene);
         stage.show();
     }
+
 }
 
 
